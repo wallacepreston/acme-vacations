d3460d1d62c52aea7f2d4dea5cdd2b58a52c1634
Preston Wallace <wallace.preston@gmail.com>
2020-01-25 13:05:40 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
+const apiUrl = 'https://acme-users-api-rev.herokuapp.com/api'
+
 function App() {
+  const [user, setUser] = useState({});
+  const [vacations, setVacations] = useState([]);
+  const fetchUser = async () => {
+    const appStorage = window.localStorage;
+    const userId = appStorage.getItem('userId');
+    let user;
+    if(userId) {
+      try {
+        user = (await axios.get(`${apiUrl}/users/detail/${userId}`)).data;
+      } catch (err) {
+        appStorage.removeItem('userId');
+        return fetchUser();
+      }
+    } else {
+      user = (await axios.get(`${apiUrl}/users/random`)).data;
+      appStorage.setItem('userId', user.id)
+    }
+    console.log(user);
+    setUser(user)
+  }
+  const fetchVacations = async () => {
+    if(!user.id) return;
+    const response = await axios.get(`${apiUrl}/users/${user.id}/vacations`);
+    console.log(response);
+    setVacations(response.data);
+  }
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  useEffect(() => {
+    fetchVacations();
+  }, [user])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Acme Vacations</h1>
+        <div>Name: {user && user.fullName}</div>
+        <div>Vacations: {vacations && vacations.map(vacation => {
+          const startDate = new Date(vacation.startDate);
+          const endDate = new Date(vacation.endDate);
+          const numDays = endDate.getDay() - startDate.getDay();
+          return (
+            <div key={vacation.id}>
+              {startDate.toDateString()} to {endDate.toDateString()} ({numDays} days)
+
+            </div>
+          )
+        }
+        
+        )}</div>
+      </main>
     </div>
   );
 }
